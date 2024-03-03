a483ed21b9034a268977d72931b4573e2f16bd1e
Kodachi <salaheddine.mahraz@proton.me>
2024-03-03 12:55:11 +0000

    add read me

---
+++ README.md
@@ -0,0 +1,16 @@
+# OptionShell
+
+## Purpose:
+I tried to use the built-in module `argparse`, and I didn't like working with it for many reasons, such as:
+- I can't try-catch the errors: When an error happens, it just exits, and I can't handle it.
+   ```
+   usage: example.py [-h] [--foo FOO] [--bar BAR]
+   example.py: error: argument --foo: not allowed with argument --bar
+   ```
+So, I made this script.
+
+## Features:
+- It will support auto-completion.
+- You can customize it.
+
+It's my first script, so go easy on me. 
--- optiShell.py
+++ optiShell.py
@@ -1,7 +1,72 @@
 
 
+
 class OptiShell:
-    def __init__(self, , ignoreAddtions=False):
+    def __init__(self, ignoreDouble=True, ignoreAddtions=False):
+        from sys import argv
+        self.argument = argv
+    def add(self, *flags, help:str='', type=str, action:str='store'):
+        argLen = len(flags)
+        
+        # if *flags was empty will raise an error.
+        if argLen == 0:
+            raise TypeError("add() get at least one flag or positionl_argument") 
+        # check if it's a flag, positionlArgument or empty string.
+        elif argLen == 1:
+            if flags[0] = '':# empty string
+                raise ValueError("did not give add any flag or positionl_argument")
+            OptiShell._positionalORflag() 
+            pass
+        # add() function can not contain more than one positionlArgument
+        if argLen >= 2:
+            # argLen  > 1 that means there is no positionlArguments, cause add() can take only one
+            # look for non-wanted situation like: 's-' '---something' '-flagFullForm' and mix between flags and positionlArguments
+            OptiShell._avoidPositional(args)
+        del argLen
+        validTypes = (
+            bool,
+            str, 
+            int, 
+            list, 
+            tuple, 
+            float, 
+            set
+        )
+        actionsList = (
+            'store',
+            'store_true',
+            'store_false',
+            'append',
+            'append_const',
+            'count',
+            'help'
+        )
+        if not type in validTypes:
+            raise ValueError(f"Invalid type: {type}. Valid types are: {validTypes}")    
+        elif not action in actionsList:
+            raise ValueError(f"Invalid action: {action}. Valid actions are: {actionsList}")
+        
+    def _get():
         pass
-    def add(*args,help='',type='bool'):
+    def _avoidPositional(flags) -> list:
+        for f in flags:
+            dashes = f.count('-')
+            if dashes == 0:
+                raise ValueError(f'Invalid type: {f} should starts with "-"; \n{f} is not positional')
+            elif dashes > 2;
+                pass
+
+
+
+    def help():
         pass
+
+
+        
+       
+
+
+
+if __name__=='__main__':
+    OptiShell().add(type=set,)
+
